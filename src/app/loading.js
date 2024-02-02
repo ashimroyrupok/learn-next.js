fcf6b272ec7774b7f0357fd76a8607143db953e2
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-02 22:19:20 +0600

    learn loading page

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+
+const LoadingPage = () => {
+    return (
+        <div>
+
+            <h2 className="">Loading..........</h2>
+            
+        </div>
+    );
+};
+
+export default LoadingPage;
---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+
+            <h3> 404!!!! Not found </h3>
+            
+        </div>
+    );
+};
+
+export default NotFoundPage;
