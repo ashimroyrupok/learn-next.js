8ba736bc5596563923e6db620283e0192e3773e9
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-02 22:42:51 +0600

    learn navigation using link

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Counter from '@/components/Counter/Counter';
+import Link from 'next/link';
 import React from 'react';
 
 const HomePage = () => {
   return (
    
     <div>
+      <div>
+        <Link className='btn btn-primary' href={'/about'}> about </Link>
+        <Link className='btn' href={'/contact'}>contact</Link>
+      </div>
       <h2>Hello Next.js</h2>
       <Counter/>
     </div>
