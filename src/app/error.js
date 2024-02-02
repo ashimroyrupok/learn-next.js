6dc4ba20451114c9eefb81836653d64019603717
ashimroyrupok <ashimroyrupok@gmail.com>
2024-02-02 22:22:36 +0600

    implement error

---
+++ src/app/error.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <p className='text-red-600 text-3xl'> something went wrong. </p>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const HomePage = () => {
   return (
+   
     <div>
       <h2>Hello Next.js</h2>
       <Counter/>
